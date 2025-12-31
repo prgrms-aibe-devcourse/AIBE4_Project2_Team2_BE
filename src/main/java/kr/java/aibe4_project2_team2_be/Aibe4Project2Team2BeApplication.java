f0dd8099f0f6ce25b0515c0a2112384f8c8e3345
ehdghksahd <ehdghksahd@naver.com>
2025-12-31 12:58:58 +0900

    251231_init

---
+++ src/main/java/kr/java/aibe4_project2_team2_be/Aibe4Project2Team2BeApplication.java
@@ -0,0 +1,13 @@
+package kr.java.aibe4_project2_team2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aibe4Project2Team2BeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aibe4Project2Team2BeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/java/aibe4_project2_team2_be/Aibe4Project2Team2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.java.aibe4_project2_team2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aibe4Project2Team2BeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
