18fa21fdb73c699d3ae61f7bca553533bc383815
PARK HYEONG MIN <qd0420@gmail.com>
2026-01-06 10:36:40 +0900

    //

--- src/main/java/kr/java/aibe4_project2_team2_be/majormate/MajorMateApplication.java
+++ src/main/java/kr/java/aibe4_project2_team2_be/majormate/MajorMateApplication.java
@@ -2,7 +2,7 @@ package kr.java.aibe4_project2_team2_be.majormate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
 @SpringBootApplication
 public class MajorMateApplication {
 
