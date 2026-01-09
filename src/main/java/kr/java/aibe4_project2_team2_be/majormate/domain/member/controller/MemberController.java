4fe51ba35fdabcd78c7839d39e2342d540d72dcf
한재호 <wogh009@gmail.com>
2026-01-09 16:24:27 +0900

    feat : dev 병합 후 email 인증 및 아이디/비밀번호 찾기 구현

--- src/main/java/kr/java/aibe4_project2_team2_be/majormate/domain/member/controller/MemberController.java
+++ src/main/java/kr/java/aibe4_project2_team2_be/majormate/domain/member/controller/MemberController.java
@@ -13,6 +13,7 @@ import kr.java.aibe4_project2_team2_be.majormate.domain.member.dto.response.Memb
 import kr.java.aibe4_project2_team2_be.majormate.domain.member.dto.response.MemberProfileResponse;
 import kr.java.aibe4_project2_team2_be.majormate.domain.member.service.MemberService;
 import kr.java.aibe4_project2_team2_be.majormate.global.common.response.ApiResponse;
+import kr.java.aibe4_project2_team2_be.majormate.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -24,32 +25,28 @@ public class MemberController {
 
 	@GetMapping
 	public ApiResponse<MemberProfileResponse> getMyProfile() {
-		//Long memberId = SecurityUtil.getCurrentMemberId();
-		Long memberId = 9L;
+		Long memberId = SecurityUtil.getCurrentMemberId();
 		MemberProfileResponse response = memberService.getMemberProfile(memberId);
 		return ApiResponse.success(response);
 	}
 
 	@GetMapping("/academic")
 	public ApiResponse<MemberAcademicResponse> getMyAcademic() {
-		//Long memberId = SecurityUtil.getCurrentMemberId();
-		Long memberId = 9L;
+		Long memberId = SecurityUtil.getCurrentMemberId();
 		MemberAcademicResponse response = memberService.getMemberAcademic(memberId);
 		return ApiResponse.success(response);
 	}
 
 	@GetMapping("/detail")
 	public ApiResponse<MemberDetailResponse> getMyDetail() {
-		//Long memberId = SecurityUtil.getCurrentMemberId();
-		Long memberId = 9L;
+		Long memberId = SecurityUtil.getCurrentMemberId();
 		MemberDetailResponse response = memberService.getMemberDetail(memberId);
 		return ApiResponse.success(response);
 	}
 
 	@PatchMapping("/detail")
 	public ApiResponse<MemberDetailResponse> updateMyDetail(@RequestBody @Valid MemberDetailUpdateRequest request) {
-		//Long memberId = SecurityUtil.getCurrentMemberId();
-		Long memberId = 9L;
+		Long memberId = SecurityUtil.getCurrentMemberId();
 		MemberDetailResponse response = memberService.updateMemberDetail(memberId, request);
 		return ApiResponse.success(response);
 	}
