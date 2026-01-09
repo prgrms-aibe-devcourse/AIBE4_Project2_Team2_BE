75049788ae95baf106959c412de4c53f6b0efd09
PARK HYEONG MIN <qd0420@gmail.com>
2026-01-09 17:51:14 +0900

    [Feature] thymeleaf 승인 반려 성공 1. 승인 반려 service 성공 2. 활동 정지 신청 추가 예정 3. 자격 박탈 추가 예정

--- src/main/java/kr/java/aibe4_project2_team2_be/majormate/domain/admin/repository/AdminRequestStatusHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.java.aibe4_project2_team2_be.majormate.domain.admin.repository;
-
-import kr.java.aibe4_project2_team2_be.majormate.domain.major_role_request.entity.RequestStatusHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AdminRequestStatusHistoryRepository extends JpaRepository<RequestStatusHistory, Long> {
-}
